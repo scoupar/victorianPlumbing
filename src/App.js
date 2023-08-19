31c7d9d64be9d51311daa838c3bba8fafe65715e
Scott Coupar <scott.coupar@googlemail.com>
2023-08-19 13:03:50 +0100

    first commit - fetched products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    const response = await fetch(
+      "https://spanishinquisition.victorianplumbing.co.uk/interviews/listings?apikey=yj2bV48J40KsBpIMLvrZZ1j1KwxN4u3A83H8IBvI",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: "toilets",
+          pageNumber: 0,
+          size: 0,
+          additionalPages: 0,
+          sort: 1,
+        }),
+      }
+    );
+    const data = await response.json();
+    setProducts(data.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  // console.log(getProducts());
+
+  // getProducts();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
     </div>
   );
-}
+};
 
 export default App;
